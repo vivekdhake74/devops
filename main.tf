2dc119e48a210e10a028bb15838d93821d7e4d3a
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 13:35:19 +0530

    added aws infra

--- main.tf
+++ main.tf
@@ -16,29 +16,3 @@ resource "aws_subnet" "private" {
   })
 }
 
-resource "aws_lambda_function" "lambda" {
-  function_name = "invoke_lambda"
-  filename         = data.archive_file.zip.output_path
-  source_code_hash = data.archive_file.zip.output_base64sha256
-  role    = aws_iam_role.iam_for_lambda.arn
-  handler = "lambda_function.lambda_handler"
-  runtime = "python3.6"
-}
-
-data "aws_lambda_invocation" "lambda1" {
-  function_name = aws_lambda_function.lambda_function_test.function_name
-
-  input = <<JSON
-{
-  payload = {
-  "subnet_id": "private-subnet-1",
-  "name": "vivekdhake",
-  "email": "vivekdhake86@gmail.com"
- }
-}
-JSON
-}
-
-output "result_entry" {
-  value = jsondecode(data.aws_lambda_invocation.example.result)["key1"]
-}
