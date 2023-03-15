03f484471de99bb2f0dccef86613898470a2280e
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 12:01:56 +0530

    added private subnet

--- main.tf
+++ main.tf
@@ -5,3 +5,13 @@ terraform {
     region = "eu-west-1"
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = merge(var.project_tags, {
+    Name = "private-subnet-1"
+  })
+}
