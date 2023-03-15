cb7df4df64855724d115bad6851f83ddb446e160
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 14:13:34 +0530

    added lambda function

--- main.tf
+++ main.tf
@@ -10,5 +10,16 @@ resource "aws_subnet" "private" {
   vpc_id            = "vpc-0de2bfe0f5fc540e0"
   cidr_block        = "10.0.1.0/24"
 
+  tags = merge(var.project_tags, {
+    Name = "private-subnet-1"
+  })
 }
 
+resource "aws_lambda_function" "lambda" {
+  function_name = "invoke_lambda"
+  filename         = data.archive_file.zip.output_path
+  source_code_hash = data.archive_file.zip.output_base64sha256
+  role    = aws_iam_role.iam_for_lambda.arn
+  handler = "lambda_function.lambda_handler"
+  runtime = "python3.6"
+}
