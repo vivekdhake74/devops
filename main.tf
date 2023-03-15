5447bf387ce96d85495af1163b0f353b0edf8dd2
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 14:30:55 +0530

    added tag

--- main.tf
+++ main.tf
@@ -9,9 +9,8 @@ terraform {
 resource "aws_subnet" "private" {
   vpc_id            = "vpc-0de2bfe0f5fc540e0"
   cidr_block        = "10.0.1.0/24"
-
   tags = merge(var.project_tags, {
     Name = "private-subnet-1"
   })
-}
 
+}
