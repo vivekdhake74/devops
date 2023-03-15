9e486cc4401f0f8a58ef6dbff5ae6d09a69893ce
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 15:22:25 +0530

    removed role

--- main.tf
+++ main.tf
@@ -20,7 +20,6 @@ data "archive_file" "lambda" {
 resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
-  role          = aws_iam_role.iam_for_lambda.arn
   handler       = "index.test"
 
   source_code_hash = data.archive_file.lambda.output_base64sha256
