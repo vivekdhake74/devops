4ab13aa003e49ba995a55c715e180e263a9f3f81
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 13:46:05 +0530

    added subnet

--- main.tf
+++ main.tf
@@ -10,8 +10,5 @@ resource "aws_subnet" "private" {
   vpc_id            = "vpc-0de2bfe0f5fc540e0"
   cidr_block        = "10.0.1.0/24"
 
-  tags = merge(var.project_tags, {
-    Name = "private-subnet-1"
-  })
 }
 
