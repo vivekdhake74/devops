b526dc4f3a7083081e55022868ed63776cfd59fe
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 14:24:16 +0530

    working on pipeline

--- main.tf
+++ main.tf
@@ -15,11 +15,3 @@ resource "aws_subnet" "private" {
   })
 }
 
-resource "aws_lambda_function" "lambda" {
-  function_name = "invoke_lambda"
-  filename         = data.archive_file.zip.output_path
-  source_code_hash = data.archive_file.zip.output_base64sha256
-  role    = aws_iam_role.iam_for_lambda.arn
-  handler = "lambda_function.lambda_handler"
-  runtime = "python3.6"
-}
