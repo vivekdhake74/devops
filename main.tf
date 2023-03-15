0137511151dac0c31555ed9486ccbf77b8d46f36
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 15:34:36 +0530

    added role

--- main.tf
+++ main.tf
@@ -11,11 +11,6 @@ resource "aws_subnet" "private" {
   cidr_block        = "10.0.1.0/24"
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
-  name               = "iam_for_lambda"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda.js"
@@ -26,7 +21,7 @@ resource "aws_lambda_function" "test_lambda" {
   filename      = "lambda_function_payload.zip"
   function_name = "lambda_function_name"
   handler       = "index.test"
-  role          = aws_iam_role.iam_for_lambda.arn
+  role          =  aws_iam_role.lambda
   source_code_hash = data.archive_file.lambda.output_base64sha256
 
   runtime = "nodejs16.x"
