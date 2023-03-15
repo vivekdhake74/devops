a8502f9e230f02559e1105a84b82e784ea0f117c
vivekdhake74 <vivekdhake74@gmail.com>
2023-03-15 12:57:05 +0530

    added lambda

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ resource "aws_lambda_function" "lambda" {
   filename         = data.archive_file.zip.output_path
   source_code_hash = data.archive_file.zip.output_base64sha256
   role    = aws_iam_role.iam_for_lambda.arn
-  handler = "welcome.lambda_handler"
+  handler = "lambda_function.lambda_handler"
   runtime = "python3.6"
 }
 
